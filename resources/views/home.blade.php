b5fa443ceb255122e7c68845529d3abe338da105
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-05-10 15:41:52 +0200

    [add] Print Movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,15 @@
         <div class="row row-cols-3">
 
             @foreach ($movies as $movie)
-                <div class="col">
-                    <h1>{{ $movie->title }}</h1>
+                <div class="col mb-3">
+                    <div class="card h-100 text-center">
+                        <h3>Titolo: {{ $movie->title }}</h3>
+                        <h5>Titolo originale: {{ $movie->original_title }}</h5>
+                        <h5>Nazionalità: {{ $movie->nationality }}</h5>
+                        <h5>Data d'uscita: {{ $movie->date }}</h5>
+                        <h5>Voto: {{ $movie->vote }}</h5>
+
+                    </div>
                 </div>
             @endforeach
 
